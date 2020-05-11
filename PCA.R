a76d38189daffff909260504e7b407cc8a3fab8c
Educauchy <educauchy@gmail.com>
2020-05-11 23:48:47 +0300

    Complete SFA model

--- PCA.R
+++ PCA.R
@@ -14,6 +14,7 @@ input <- c('X1.1', 'X1', 'X9', 'X2.7', 'X2.16', 'X3.8', 'X3.9', 'X35', 'X4.1', '
 input_positive_cor <- c('X2.7', 'X5.1', 'X6.1_2', 'X6.4', 'X35', 'X40_41_42', 'X48')
 input_negative_cor <- c('X1', 'X2.1_2_3', 'X2.4_5_6', 'X3.8', 'X3.9', 'X4.3', 'X5.6', 'X9', 'X28')
 output_cor <- c('E.1', 'E.2', 'E.4', 'E.5')
+output_cor_all <- c('E.1', 'E.2', 'E.3', 'E.4', 'E.5')
 
 
 # PCA Input Positive
@@ -64,3 +65,8 @@ dev.off()
 png(paste0(thesis_url, 'Media/PCA/Output_Biplot.png'), width = 500, height = 500)
 ggbiplot(pca_out, labels = uni)
 dev.off()
+
+
+# PCA Output All
+pca_out <- prcomp(data[, output_cor_all])
+summary(pca_out)
